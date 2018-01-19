0cd51850843e1539e9bfb52c63a2275074cc3e1c
zorail <rohan.seth497@gmail.com>
2018-01-20 03:01:04 +0530

    demo user model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Represent a user profile inside our system"""
+
+    email = models.EmailField(max_length=50, unique=True)
+    name = models.CharField(max_length=255)
+
+    # Required
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+
+    # //Required for django admin
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email       
